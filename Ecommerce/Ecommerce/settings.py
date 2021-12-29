10537816703c25f782d8097b8925d4660d377c88
istiak1992 <istiak.uiu.bd@gmail.com>
2021-12-30 00:29:48 +0600

    third commit

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -122,14 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT =  BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 
 STATICFILES_DIRS = ( 
-    MEDIA_ROOT,        
+    STATIC_ROOT,        
 )
 
 
